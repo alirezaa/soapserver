3e4301ece507c83f663a38b5e729508ff9fce01a
unknown <aghamoa@yahoo.com>
2012-01-16 19:59:49 -0500

    first commit

---
+++ WS-Server/WS-Server/src/ali/HelloWorldPublisher.java
@@ -0,0 +1,13 @@
+package ali;
+
+import javax.xml.ws.Endpoint;
+import ali.HelloWorldImpl;
+ 
+//Endpoint publisher
+public class HelloWorldPublisher{
+ 
+	public static void main(String[] args) {
+	   Endpoint.publish("http://localhost:9999/ws/hello", new HelloWorldImpl());
+    }
+ 
+}
